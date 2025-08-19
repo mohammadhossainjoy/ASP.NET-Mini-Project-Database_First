a689bfdc879c788dbe7ce4e81a1d4a196e0f550a
mohammadhossainjoy <joyh2755@gmail.com>
2025-08-20 02:09:31 +0600

    Updated only RouteConfig.cs file

--- HR/App_Start/RouteConfig.cs
+++ HR/App_Start/RouteConfig.cs
@@ -14,10 +14,11 @@ namespace HR
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
